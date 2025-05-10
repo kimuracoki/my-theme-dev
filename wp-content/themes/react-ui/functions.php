9893af9cfd4feaf108f33afa2289e3bc351e8f0e
kimura.coki <kimura.coki@gmail.com>
2025-05-11 01:04:23 +0900

    Add Custom

--- wp-content/themes/react-ui/functions.php
+++ wp-content/themes/react-ui/functions.php
@@ -1,4 +1,6 @@
 <?php
+
+// Vite でビルドした React のアセットを読み込む
 function mytheme_enqueue_assets() {
   wp_enqueue_style('mytheme-style', get_stylesheet_uri());
 
@@ -31,6 +33,43 @@ function mytheme_enqueue_assets() {
       null,
       true
     );
+
+    // React 側にカスタマイズデータを渡す
+    wp_localize_script('mytheme-react-app', 'mythemeData', [
+      'heroTitle' => get_theme_mod('hero_title', 'ようこそ！'),
+      'heroImage' => get_theme_mod('hero_image', ''),
+    ]);
   }
 }
 add_action('wp_enqueue_scripts', 'mytheme_enqueue_assets');
+
+
+// カスタマイザーに設定項目を追加
+function mytheme_customize_register($wp_customize) {
+  // ヒーロータイトル
+  $wp_customize->add_setting('hero_title', [
+    'default' => 'ようこそ！',
+    'sanitize_callback' => 'sanitize_text_field',
+  ]);
+  $wp_customize->add_control('hero_title', [
+    'label' => 'ヒーローセクションのタイトル',
+    'section' => 'title_tagline',
+    'type' => 'text',
+  ]);
+
+  // ヒーロー画像
+  $wp_customize->add_setting('hero_image', [
+    'default' => '',
+    'sanitize_callback' => 'esc_url_raw',
+  ]);
+  $wp_customize->add_control(new WP_Customize_Image_Control(
+    $wp_customize,
+    'hero_image',
+    [
+      'label' => 'ヒーロー画像',
+      'section' => 'title_tagline',
+      'settings' => 'hero_image',
+    ]
+  ));
+}
+add_action('customize_register', 'mytheme_customize_register');
