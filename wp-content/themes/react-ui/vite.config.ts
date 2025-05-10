3d5c7d7ae103223e10eeb75ae38543c517af972b
kimura.coki <kimura.coki@gmail.com>
2025-05-11 00:33:00 +0900

    Add image path

--- wp-content/themes/react-ui/vite.config.ts
+++ wp-content/themes/react-ui/vite.config.ts
@@ -2,20 +2,25 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    manifest: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, "index.html"),
-      output: {
-        assetFileNames: "assets/[name]-[hash][extname]",
-        entryFileNames: "assets/[name]-[hash].js",
-        chunkFileNames: "assets/[name]-[hash].js",
+export default defineConfig(({ command }) => {
+  const isBuild = command === "build";
+  return {
+    base: isBuild
+      ? "/wp-content/themes/react-ui/dist/" 
+      : "/",
+    plugins: [react()],
+    build: {
+      outDir: "dist",
+      emptyOutDir: true,
+      manifest: true,
+      rollupOptions: {
+        input: path.resolve(__dirname, "index.html"),
+        output: {
+          assetFileNames: "assets/[name]-[hash][extname]",
+          entryFileNames: "assets/[name]-[hash].js",
+          chunkFileNames: "assets/[name]-[hash].js",
+        },
       },
     },
-  },
+  };
 });
